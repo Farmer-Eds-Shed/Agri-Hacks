0054cf4455bb55c3f1c1a3f685ab078d8bb8278c
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-01-21 11:53:02 +0000

    Deploy to Heroku

--- agri_hacks/settings.py
+++ agri_hacks/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xcxz3euoaw*8utoi=xkwalw6w^w!&yx1@(@7ppwg^v#%*pe=dv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
