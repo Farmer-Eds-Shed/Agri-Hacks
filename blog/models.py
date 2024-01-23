0acba977301a565ba748e5c4e45da5fed7aa3d2e
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-01-23 19:08:08 +0000

    Add meta data to posts

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,13 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+    
     
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
