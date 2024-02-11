2055f1ce168d3a7fcf498947b546e3beea2312db
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-02-11 10:40:43 +0000

    Move JS from bottom of HTML to static files

--- staticfiles/js/post_detail.js
+++ staticfiles/js/post_detail.js
@@ -45,3 +45,26 @@ for (let button of editButtons) {
         deleteModal.show();
     });
 }
+
+/*Function to get CSRF token*/
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrfToken = getCookie('csrftoken');
+
+/*likes_area js, uses HTMX to avoid reloading page on like/made_this clicks*/
+document.body.addEventListener('htmx:configRequest', (e) => {
+    e.detail.headers['X-CSRFToken'] = csrfToken;
+  })
